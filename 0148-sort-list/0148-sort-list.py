c4e48390f5570cccab5a660c53ec471ac009526c
ARCHIET VISWAJIT TANWAR <archiettanwar71@gmail.com>
2026-08-21 21:07:04 +0530

    Time: 192 ms (22.76%), Space: 40.8 MB (16.03%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -2,38 +2,38 @@ class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return head
-
-        def get_mid(node):
-            slow = node
-            fast = node.next
+        
+        def get_mid(head):
+            slow,fast=head,head.next
             while fast is not None and fast.next is not None:
-                slow = slow.next
-                fast = fast.next.next
+                slow=slow.next
+                fast=fast.next.next
             return slow
         
-        def mergeList(left_node, right_node):
-            dummy = ListNode(0)
-            tail = dummy
-            while left_node is not None and right_node is not None:
-                if left_node.val <= right_node.val:
-                    tail.next = left_node
-                    left_node = left_node.next
+        def merge(left,right):
+            dummy=ListNode(0)
+            tail=dummy
+            while left is not None and right is not None:
+                if left.val<=right.val:
+                    tail.next=left 
+                    left=left.next
                 else:
-                    tail.next = right_node
-                    right_node = right_node.next
-                tail = tail.next
-            if left_node:
-                tail.next = left_node 
-            if right_node:
-                tail.next = right_node
+                    tail.next=right
+                    right=right.next
+                tail=tail.next
+            if left:
+                tail.next=left
+            if right:
+                tail.next=right
             return dummy.next
-            
-        left = head
-        mid = get_mid(head)
-        right = mid.next
-        mid.next = None
+        
+        left=head
+        right=get_mid(head)
+        tmp=right.next
+        right.next=None
+        right=tmp
 
-        left = self.sortList(left)
-        right = self.sortList(right)
+        left=self.sortList(left)
+        right=self.sortList(right)
 
-        return mergeList(left, right)
+        return merge(left,right)
