00c4af28fc7aa38bedb02f7573379b28bb679963
ARCHIET VISWAJIT TANWAR <archiettanwar71@gmail.com>
2026-08-21 20:17:07 +0530

    Time: 0 ms (100%), Space: 12.4 MB (84.16%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        dummy=ListNode(0)
+        tail=dummy
+        l1=list1
+        l2=list2
+        while l1 and l2:
+            if l1.val<=l2.val:
+                tail.next=l1
+                l1=l1.next
+            else:
+                tail.next=l2
+                l2=l2.next
+            tail=tail.next
+        if l1 :
+            tail.next=l1
+        if l2:
+            tail.next=l2
+        return dummy.next
