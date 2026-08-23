b3984c87dac3ece31b9bcb2b8aafc0f282c14e37
ARCHIET VISWAJIT TANWAR <archiettanwar71@gmail.com>
2026-08-23 14:56:13 +0530

    Time: 4 ms (5.81%), Space: 19.2 MB (95.57%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -2,21 +2,18 @@ class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         if not head or not head.next or k==0:
             return head
-        
         length=1
         tail=head
         while tail.next:
             tail=tail.next
             length+=1
-            
         tail.next=head
 
         k=k%length
-
         steps_to_tail=length-k-1
-        new_tail=head
 
-        for _ in range(steps_to_tail):
+        new_tail=head
+        for i in range(steps_to_tail):
             new_tail=new_tail.next
         
         new_head=new_tail.next
