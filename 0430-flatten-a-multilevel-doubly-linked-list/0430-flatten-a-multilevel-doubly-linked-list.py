62407de5c3798a34810ad55b7493561da149c310
ARCHIET VISWAJIT TANWAR <archiettanwar71@gmail.com>
2026-08-25 23:15:22 +0530

    Time: 45 ms (72.71%), Space: 19.8 MB (69.97%) - LeetHub

---
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.py
@@ -0,0 +1,21 @@
+class Solution:
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+        
+        curr = head
+        while curr:
+            if curr.child:
+                tail = curr.child
+                while tail.next:
+                    tail = tail.next
+                if curr.next:
+                    curr.next.prev = tail
+                    tail.next = curr.next
+                curr.next = curr.child
+                curr.child.prev = curr
+                
+                curr.child = None
+            curr = curr.next
+            
+        return head
