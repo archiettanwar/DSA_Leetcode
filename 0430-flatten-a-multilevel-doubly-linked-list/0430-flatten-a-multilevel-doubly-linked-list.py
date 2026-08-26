3849d3415e6ac9f8b55643bbbe85f847bc24fc5d
ARCHIET VISWAJIT TANWAR <archiettanwar71@gmail.com>
2026-08-26 13:15:24 +0530

    Time: 53 ms (21.5%), Space: 19.9 MB (69.71%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.py
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.py
@@ -2,20 +2,17 @@ class Solution:
     def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return None
-        
-        curr = head
+        curr=head
         while curr:
             if curr.child:
-                tail = curr.child
+                tail=curr.child
                 while tail.next:
-                    tail = tail.next
+                    tail=tail.next
                 if curr.next:
-                    curr.next.prev = tail
-                    tail.next = curr.next
-                curr.next = curr.child
-                curr.child.prev = curr
-                
-                curr.child = None
-            curr = curr.next
-            
+                    curr.next.prev=tail
+                    tail.next=curr.next
+                curr.next=curr.child
+                curr.child.prev=curr
+                curr.child=None
+            curr=curr.next
         return head
