ace339e4a47a5fb526ea3319fb1d2636948bb4e9
ARCHIET VISWAJIT TANWAR <archiettanwar71@gmail.com>
2026-09-03 21:20:54 +0530

    Time: 0 ms (100%), Space: 19.5 MB (6.04%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -2,12 +2,14 @@ class Solution:
     def nextGreaterElement(self, nums1: list[int], nums2: list[int]) -> list[int]:
         hmap={}
         stack=[]
-        res=[]
+
         for i in nums2:
-            while stack and i > stack[-1]:
-                popped = stack.pop()
-                hmap[popped] = i
+            while stack and i>stack[-1]:
+                popped=stack.pop()
+                hmap[popped]=i
             stack.append(i)
+        res=[]
         for i in nums1:
             res.append(hmap.get(i,-1))
+        
         return res
